7e8ae59dede6680da2b3f1bfc8421f501631400e
ssannkkkii <oleksandrhredil2005@gmail.com>
2025-05-05 18:30:13 +0300

    create navbar

--- frontend/src/pages/MainPage/Header/Header.jsx
+++ frontend/src/pages/MainPage/Header/Header.jsx
@@ -1,25 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classes from './../MainPage.module.scss';
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(prev => !prev);
+  };
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
   return (
     <header className={classes.header}>
       <div className={classes.container}>
         <div className={classes.header__row}>
           <div className={classes.header__logo}>
-            <p>MFinance</p>
-            </div>
-          <div className={classes.header__nav}>
+            MFinance
+          </div>
+
+          <button className={`${classes.burger} ${menuOpen ? classes.open : ''}`} onClick={toggleMenu}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
+
+          <nav className={`${classes.header__nav} ${menuOpen ? classes.open : ''}`}>
             <div className={classes.header__nav__link}>
-              <a href="#" className={classes.login}>Log in</a>
+              <a href="#" onClick={closeMenu}>Log in</a>
             </div>
             <div className={classes.header__nav__btn}>
-              <a href="#" className={classes.button}>Get started</a>
+              <a href="#" className={classes.button} onClick={closeMenu}>Get started</a>
             </div>
-          </div>
+          </nav>
         </div>
       </div>
-	  </header>
+
+      {menuOpen && <div className={classes.overlay} onClick={closeMenu}></div>}
+    </header>
   );
 };
 
